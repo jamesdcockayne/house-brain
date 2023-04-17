d7dbb63a31e2d2e99f34c692b279ac8b382d2a7d
jamesdcockayne <jamesdcockayne@googlemail.com>
2023-04-17 16:19:09 +0100

    Add concreate IImmersionRelay implementation

---
+++ Service/Service/Immersion/ImmersionRelay.cs
@@ -0,0 +1,51 @@
+﻿using System.Device.Gpio;
+using Microsoft.Extensions.Options;
+
+namespace Service.Immersion;
+
+public class ImmersionRelay : IImmersionRelay, IDisposable
+{
+    private bool disposedValue;
+    private readonly GpioController _gpioController;
+    private readonly WiringPinOptions _wiringPinOptions;
+
+    public ImmersionRelay(IOptions<WiringPinOptions> options)
+    {
+        _gpioController = new GpioController(PinNumberingScheme.Logical);
+        _wiringPinOptions = options.Value;
+
+        _gpioController.OpenPin(options.Value.ImmersionRelayPinNumber, PinMode.Output);
+    }
+
+
+    public bool TopImmersionEnabled
+    {
+        get => _gpioController.Read(_wiringPinOptions.ImmersionRelayPinNumber) == PinValue.High;
+        set
+        {
+            PinValue pinValue = value ? PinValue.High : PinValue.Low;
+
+            _gpioController.Write(_wiringPinOptions.ImmersionRelayPinNumber, pinValue);
+        }
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                _gpioController.Dispose();
+            }
+
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ Service/Service/WiringPinOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Service;
+
+public class WiringPinOptions
+{
+    public int ImmersionRelayPinNumber { get; set; }
+}
+
+// epaper hat
+//VCC    ->    3.3
+//GND    ->    GND
+//DIN    ->    10(SPI0_MOSI)
+//CLK    ->    11(SPI0_SCK)
+//CS     ->    8(SPI0_CS0)
+//DC     ->    25
+//RST    ->    17
+//BUSY   ->    24
+// https://github.com/waveshare/e-Paper/blob/master/RaspberryPi_JetsonNano/python/readme_rpi_EN.txt
